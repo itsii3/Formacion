775390dc0754f3d8fdcdbe2c4fde78bd5c48397a
Ivet <ivetacosta03@gmail.com>
2021-10-13 16:06:53 +0200

    Disposicion en Storyboard + Struct Empleado

---
+++ Empleado/Empleado.swift
@@ -0,0 +1,17 @@
+//
+//  Empleado.swift
+//  Empleado
+//
+//  Created by Ivet Acosta on 13/10/2021.
+//
+
+import UIKit
+
+struct Empleado {
+    
+    var nombre: String
+    var cargo: String
+    var departamento: String
+    var edad: Int
+    
+}
